05686830bbd88515a24a114e24e17b003da4ea77
Dmytro <hellohowareyouuuu@gmail.com>
2018-06-10 17:21:51 +0300

    add menu

--- app/js/src.js
+++ app/js/src.js
@@ -15,3 +15,32 @@ hammertime.on('panleft panright', function(ev) {
     $('.main-menu').removeClass('active');
   }
 });
+
+// function setParentHeight(el){
+//   var parent = el.parent()
+//   parent.height(el.height())
+// }
+$('[data-tabtrigger]').click(function(e){
+  e.preventDefault();
+  var n = $(this).data('tabtrigger');
+  var group = $(this).data('group');
+  if($(this).is('[data-toggle]')){
+    if($(this).is('.active')){
+      $(this).removeClass('active');
+      $('[data-tab="'+n+'"][data-group="'+group+'"]').removeClass('active');
+    }else{
+      $(this).addClass('active');
+      $('[data-tab="'+n+'"][data-group="'+group+'"]').addClass('active');
+    }
+  }else{
+    $('[data-group="'+group+'"]').removeClass('active')
+  $(this).addClass('active')
+  $('[data-tab="'+n+'"][data-group="'+group+'"]').addClass('active');
+  }
+   
+  // if(!$(this).is('[data-noheight]')){
+  //   setParentHeight( $('[data-tab="'+n+'"][data-group="'+group+'"]'))
+  // }
+  
+})
+$('ul.tab-nav li:first-child a').click();
